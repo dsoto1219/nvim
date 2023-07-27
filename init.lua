9998941cad51ad8cfd2d6c480e96511c33bbcdec
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2023-07-26 22:07:59 -0400

    forgot to change init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,2 @@
 require("config")
-require("lazy-plugins") -- can't write rqeuire("lazy") or else neovim gets confused between lazy module and lazy plugin
+require("plugins") -- can't write rqeuire("lazy") or else neovim gets confused between lazy module and lazy plugin
