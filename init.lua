67a2880c2ac8063d38c3a59fccf0ff050143a77d
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2023-07-27 11:55:01 -0400

    only one init.lua file now

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
-require("config")
+-- config
+vim.api.nvim_create_autocmd("User", {
+	pattern = "VeryLazy",
+	callback = function()
+		require("config.keymaps")
+		require("config.autocmds")
+	end,
+})
+require("config.options")
+
+-- plugin files
 require("plugins") -- can't write require("lazy") or else neovim gets confused between lazy module and lazy plugin
 require("cmp-setup")
--- lua/config/init.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd("User", {
-	pattern = "VeryLazy",
-	callback = function()
-		require("config.keymaps")
-		require("config.autocmds")
-	end,
-})
-
-require("config.options")
