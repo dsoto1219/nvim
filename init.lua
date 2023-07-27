68b93b905a65aa282dca83dc818ce747fa73ce40
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2023-07-27 11:47:18 -0400

    fixed cmp

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 require("config")
-require("plugins") -- can't write rqeuire("lazy") or else neovim gets confused between lazy module and lazy plugin
+require("plugins") -- can't write require("lazy") or else neovim gets confused between lazy module and lazy plugin
+require("cmp-setup")
