475ec4c4a7b77908c9b7c0188446073186663023
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2023-08-12 18:42:14 -0400

    disabled netrw and added keymap in this file

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -3,4 +3,11 @@ if not nvim_tree_status_ok then
 	return
 end
 
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+
 nvim_tree.setup()
+
+local opts = {noremap = true, silent = true}
+
+vim.keymap.set("n", "<leader>pv", ":NvimTreeFocus<CR>", opts)
