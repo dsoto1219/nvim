65068472fd7867f200e194f1a9b55fa21ca4e1ae
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2023-08-12 18:26:08 -0400

    configured bufferline, lualine, nvim-tree, and nvim-web-devicons

---
+++ after/plugin/bufferline.lua
@@ -0,0 +1,6 @@
+local bufferline_status_ok, bufferline = pcall(require, "bufferline")
+if not bufferline_status_ok then
+	return
+end
+
+bufferline.setup()
---
+++ after/plugin/nvim-tree.lua
@@ -0,0 +1,6 @@
+local nvim_tree_status_ok, nvim_tree = pcall(require, "nvim-tree")
+if not nvim_tree_status_ok then
+	return
+end
+
+nvim_tree.setup()
