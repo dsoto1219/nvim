39b6028a481d65f59d16d280fdfc8f626782b338
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2023-11-10 16:41:19 -0500

    pwsh now only opens on windows, not linux

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
+function set_options(table)
+    for k,v in pairs(table) do
+	vim.opt[k] = v
+    end
+end
+
 local options = {
     number = true,
     relativenumber = true,
@@ -25,15 +31,20 @@ local options = {
     autoindent = true,
     smartindent = false, -- https://www.reddit.com/r/vim/wiki/vimrctips/#wiki_do_not_use_smartindent
 
-    -- turn powershell on, https://github.com/akinsho/toggleterm.nvim/wiki/Tips-and-Tricks#using-toggleterm-with-powershell
-    shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-    shellquote = "",
-    shellxquote = "",
 }
 
-for k,v in pairs(options) do
-    vim.opt[k] = v
+set_options(options)
+
+if vim.loop.os_uname().sysname == "Windows" then
+    local powershell_options = {
+	-- turn powershell on, https://github.com/akinsho/toggleterm.nvim/wiki/Tips-and-Tricks#using-toggleterm-with-powershell
+	shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+	shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+	shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+	shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+	shellquote = "",
+	shellxquote = "",
+    }
+    set_options(powershell_options)
 end
+
