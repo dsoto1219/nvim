28e71478f0b58a0f17d753d18b5785a004c3ed27
thatonemailbox <127370142+thatonemailbox@users.noreply.github.com>
2023-11-19 15:08:35 -0500

    fix tabsize

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,33 +1,38 @@
 return {
-    'akinsho/toggleterm.nvim',
-    version = "*",
-    -- explicitly setup, as per documentation: https://github.com/akinsho/toggleterm.nvim#setup
-    config = function()
-	require("toggleterm").setup{
-	    size = 12,
-	    open_mapping = [[<C-\>]],
-	    autochdir = true,
-	    hide_numbers = true,
-	    direction = "horizontal",
-	    shade_terminals = true
-	    -- shade_filetypes = { "none" },
-	}
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  -- explicitly setup, as per documentation: https://github.com/akinsho/toggleterm.nvim#setup
+  config = function()
+    require("toggleterm").setup{
+      size = 12,
+      open_mapping = [[<C-\>]],
+      autochdir = true,
+      hide_numbers = true,
+      direction = "horizontal",
+      shade_terminals = true
+    }
 
-	function _G.set_terminal_keymaps()
-	    local opts = {buffer = 0}
-	    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-	    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-	    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-	    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-	    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-	    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-	end
+    function _G.set_terminal_keymaps()
+      local opts = {buffer = 0}
+      local keymap = vim.keymap.set
+      keymap('t', '<esc>', [[<C-\><C-n>]], opts)
+      keymap('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+      keymap('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+      keymap('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+      keymap('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      keymap('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+      keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+      keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+      keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
+      keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
+    end
 
-	-- if you only want these mappings for toggle term use term://*toggleterm#* instead
-	vim.cmd('autocmd TermOpen term:// lua set_terminal_keymaps()')
-
-	 --    augroup OnOpen
-		-- autocmd! 
-	 --    augroup END
-    end,
+    -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+    vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "*",
+      callback = function()
+        _G.set_terminal_keymaps()
+      end
+    })
+  end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,34 +1,34 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "c", "lua", "vim", "vimdoc" },
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup{
+      -- A list of parser names, or "all" (the five listed parsers should always be installed)
+      ensure_installed = { "c", "lua", "vim", "vimdoc" },
 
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = false,
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
 
-	-- Automatically install missing parsers when entering buffer
-	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = true,
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = true,
 
-	highlight = {
-	    enable = true,
-	    disable = { "latex" },
+      highlight = {
+	enable = true,
+	disable = { "latex" },
 
-	    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-	    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-	    -- Instead of true it can also be a list of languages
-	    additional_vim_regex_highlighting = { "latex", "markdown" },
-	},
-    },
-    config = function()
-	local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-	parser_config.haskell = {
-	  install_info = {
-	    url = "https://github.com/tree-sitter/tree-sitter-haskell",
-	    files = {"src/parser.c", "src/scanner.c", "src/unicode.h"}
-	  }
-	}
-    end
+	-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+	-- Using this option may slow down your editor, and you may see some duplicate highlights.
+	-- Instead of true it can also be a list of languages
+	additional_vim_regex_highlighting = { "latex", "markdown" },
+      },
+    }
+    local parser_config = require"nvim-treesitter.parsers".get_parser_configs()
+    parser_config.haskell = {
+      install_info = {
+	url = "https://github.com/tree-sitter/tree-sitter-haskell",
+	files = {"src/parser.c", "src/scanner.c", "src/unicode.h"}
+      }
+    }
+  end
 }
